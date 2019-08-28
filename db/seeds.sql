dbb1b370c5b71ff2e6b073691f0d9e445e8f7a38
TashaMac88 <latashammiller@gmail.com>
2019-08-28 15:39:17 -0700

    finished

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chicken');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Deluxe bacon');
+
